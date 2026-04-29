6ddc44035529272ccb35b32f570d5ff058325187
bhumi dolariya <dolariyabhumi29@gmail.com>
2026-04-29 19:33:45 +0530

    hero section

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,28 @@
+
+
+function Hero() {
+  return (
+     <section className="bg-gray-100 py-20 text-center">
+      
+      <div className="max-w-4xl mx-auto px-4">
+
+        <h1 className="text-3xl md:text-5xl font-bold text-gray-800 leading-tight">
+          Trusted by top Youtuber and Production House
+        </h1>
+
+        <p className="mt-6 text-gray-600 text-lg md:text-xl">
+          Best sound effects for youtube videos. 
+          Download high-quality audio without copyright.
+        </p>
+
+        <button className="mt-8 bg-blue-500 text-white px-6 py-3 rounded-lg text-lg hover:bg-blue-700 transition">
+          Browse Sounds
+        </button>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default Hero;
