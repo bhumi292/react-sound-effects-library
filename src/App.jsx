5125563423b2532e9e276f3e6f1c4d05d22fe32a
bhumi dolariya <dolariyabhumi29@gmail.com>
2026-05-05 12:09:08 +0530

    production

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import Header from './Components/Header'
 import Hero from './Components/Hero';
+import Production from './Components/Production';
 
 function App() {
  
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header/>
       <Hero/>
+      <Production/>
     </>
   )
 }
---
+++ src/Components/Production.jsx
@@ -0,0 +1,7 @@
+function Production() {
+  return (
+   <div className="text-2xl text-center text-black md:text-xl font-bold mt-10">Trusted by top Youtuber and Production House</div>
+  )
+}
+
+export default Production;
