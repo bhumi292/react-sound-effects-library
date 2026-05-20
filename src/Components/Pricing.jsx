22ad590cf8d0989fcc0bd43866e64b7e5a67d29c
bhumi dolariya <dolariyabhumi29@gmail.com>
2026-05-20 19:08:54 +0530

    updated

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -25,12 +25,12 @@ function Pricing() {
     <div className="bg-blue-700 py-24 px-6">
 
       {/* Heading */}
-      <h1 className="text-white text-5xl font-bold text-center">
+      <h1 className="text-white text-2xl font-bold text-center">
         Download sound effects for your project
       </h1>
 
       {/* Sub Heading */}
-      <p className="text-white text-center text-xl mt-10">
+      <p className="text-white text-center text-m mt-10">
         Find the right plan for you
       </p>
 
@@ -41,26 +41,26 @@ function Pricing() {
 
           <div
             key={item.id}
-            className={`w-[360px] h-[370px] rounded-2xl p-10 text-white ${item.bg}`}
+            className={`w-[310px] h-[350px] rounded-2xl p-5 text-white ${item.bg}`}
           >
 
             {/* Title */}
-            <h2 className="text-5xl font-bold">
+            <h2 className="text-2xl font-bold mt-auto">
               {item.title}
             </h2>
 
             {/* Description */}
-            <p className="mt-8 text-lg leading-8 text-white/90 max-w-xs">
+            <p className="mt-8 text-m leading-8 text-white/90 max-w-xs">
               {item.desc}
             </p>
 
             {/* Price */}
-            <h3 className="mt-24 text-3xl font-bold">
+            <h3 className="mt-10 text-l ">
               {item.price}
             </h3>
 
             {/* Button */}
-            <button className="mt-6 bg-white text-blue-700 font-semibold px-10 py-4 rounded-lg hover:bg-gray-100 transition">
+            <button className="mt-12 transparent text-white font-semibold px-10 py-4 rounded-lg bg-transparent border border-white/40 transition">
 
               {item.button}
 
